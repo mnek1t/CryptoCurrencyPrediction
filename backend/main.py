71b6d93d64a7922a8551ed063540d96e6931d102
mnek1t <mednikita2004@gmail.com>
2025-11-29 02:42:54 +0200

    enable cors + api of available tickers

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import joblib
 import services.polygon_io as polygon
@@ -6,7 +7,17 @@ import services.trading.technical_indicators as technical_indicators
 from constants import FEATURES
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000"
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 #model = joblib.load("eth_xgb_model.pkl")
 
 @app.get("/predict")
@@ -25,9 +36,20 @@ async def predict_price():
     # 4. Predict
     #pred = model.predict(X)[0]
     
-    # return {
-    #     "predicted_next_close": float(pred)
-    # }
+    return {
+        "predicted_next_close": 3001.42
+    }
+
+@app.get('/api/ticker')
+async def get_available_tickers():
+    return {
+        "tickers": [
+            "ETH"
+        ],
+        "currencies": {
+            "USD"
+        }
+    }
 
 def compute_indicators(df: pd.DataFrame):
     technical_indicators.calculate_atr(df)
